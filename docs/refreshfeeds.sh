e93a64041f55480e9ed0f9245a15d14c45160256
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-04-26 01:48:19 -0400

    daily feed refresh

--- docs/refreshfeeds.sh
+++ docs/refreshfeeds.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pluto build planet.ini -t news & git commit -am "daily feed refresh" & git push origin master
+pluto build planet.ini -t news & git add * & git commit -am "daily feed refresh" & git push origin master
